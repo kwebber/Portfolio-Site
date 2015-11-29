59d580bc11d784352c169a54fe01ec086af413b0
Kelly Webber <kwebber@mica.edu>
2015-11-29 08:44:31 -0500

    work from nov 28

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ $('#js-nav-toggle').on('click', function (event) {
   $('body').toggleClass('nav-is-active');
   $('#js-nav-menu').toggleClass('is-active');
 });
+
+$(document).ready(function () {
+	$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
+
+});
+
